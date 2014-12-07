49f7effc8fe7800b168bcd20766cb0e66c0a6a03
Bachar Wehbi <me@bachwehbi.net>
2014-12-07 23:47:24 +0100

    update latency examples

--- examples/latency/latency_mqtt.js
+++ examples/latency/latency_mqtt.js
@@ -10,8 +10,8 @@ var transport = {
 
 client = new bbt.Stream({ transport: transport });
 
-client.on('connected', function(c) {
-  client.subscribe( {channel: 'mychannel', resource: 'latency', read: true, write: true}, function(message){
+client.on('connected', function() {
+  client.subscribe( 'mychannel', 'latency', {read: true, write: true}, function(message){
     rtime = Date.now();
     stime = message.data.ts;
     console.log('Latency in millis: ' + ( rtime - stime ) );
@@ -20,7 +20,7 @@ client.on('connected', function(c) {
   client.on('subscribed', function(subscription) {
     //Publish a message every 1 second
     setInterval(function() {
-      client.publish({channel: 'mychannel', resource: 'latency'}, {ts: Date.now()});
+      client.publish( 'mychannel', 'latency', {ts: Date.now()});
     }, 1000);
   });
 });
