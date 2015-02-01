643fc78f6c8e7b2d98724108fcbfdee28a4fb057
Bachar Wehbi <bwehbi@beebotte.com>
2015-02-01 22:43:40 +0000

    update example

--- examples/latency/latency_socketio.js
+++ examples/latency/latency_socketio.js
@@ -3,24 +3,26 @@ var bbt = require('../../index');
 var transport = {
   type: 'socketio',
   apiKey: 'YOUR_API_ACCESS_KEY', 
-  ws_host: 'ws.beebotte.com', //default 
-  auth_endpoint: 'YOUR_AUTH_ENDPOINT', 
-  ssl: true
+  secretKey: 'YOUR_API_SECRET_KEY', 
+  ws_host: 'ws.beebotte.com', 
+  ssl: true 
 }
 
 client = new bbt.Stream({ transport: transport });
 
 client.on('connected', function() {
-  client.subscribe( 'mychannel', 'latency', {read: true, write: true}, function(message){
-    rtime = Date.now();
-    stime = message.data.ts;
-    console.log('Latency in millis: ' + ( rtime - stime ) );
+  client.on('subscribeError', function( err ) {
+    console.log( err );
   });
-
   client.on('subscribed', function(subscription) {
     //Publish a message every 1 second
     setInterval(function() {
       client.publish( 'mychannel', 'latency', {ts: Date.now()});
     }, 1000);
+  });
+  client.subscribe( 'mychannel', 'latency', {read: true, write: true}, function(message){
+    rtime = Date.now();
+    stime = message.data.ts;
+    console.log('Latency in millis: ' + ( rtime - stime ) );
   });
 });
