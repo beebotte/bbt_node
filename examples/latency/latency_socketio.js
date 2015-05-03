bd4b78d72a7525bdefdf4b8dfaade2c2be13665d
Bachar Wehbi <me@bachwehbi.net>
2015-05-04 00:51:13 +0200

    update examples

--- examples/latency/latency_socketio.js
+++ examples/latency/latency_socketio.js
@@ -2,27 +2,42 @@ var bbt = require('../../index');
 
 var transport = {
   type: 'socketio',
-  apiKey: 'YOUR_API_ACCESS_KEY', 
-  secretKey: 'YOUR_API_SECRET_KEY', 
+  apiKey: 'YOUR_API_KEY',
+  secretKey: 'YOUR_SECRET_KEY',
   ws_host: 'ws.beebotte.com', 
   ssl: true 
 }
 
-client = new bbt.Stream({ transport: transport });
+var client = new bbt.Stream({ transport: transport });
+var count = 0
+var sender;
+
+function startSender(c, to) {
+  return setInterval(function() {
+    c.publish( 'mychannel', 'latency', {ts: Date.now()});
+  }, to);
+}
+
+function stopSender(s) {
+  clearInterval(s);
+}
 
 client.on('connected', function() {
-  client.on('subscribeError', function( err ) {
-    console.log( err );
-  });
-  client.on('subscribed', function(subscription) {
-    //Publish a message every 1 second
-    setInterval(function() {
-      client.publish( 'mychannel', 'latency', {ts: Date.now()});
-    }, 1000);
-  });
   client.subscribe( 'mychannel', 'latency', {read: true, write: true}, function(message){
     rtime = Date.now();
     stime = message.data.ts;
     console.log('Latency in millis: ' + ( rtime - stime ) );
+    count++
+    //disconnect after 20 messages
+    if( count === 20) client.disconnect()
   });
+}).on('disconnected', function() {
+  stopSender(sender);
+}).on('subscribeError', function( err ) {
+  console.log( err );
+}).on('subscribed', function(subscription) {
+  //Publishes a message every 1 second
+  sender = startSender(client, 1000);
+}).on('unsubscribed', function() {
+  stopSender(sender);
 });
