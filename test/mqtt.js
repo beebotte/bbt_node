31fa8da6d1090211fcaa42c18429d5dfd920a1c1
Bachar Wehbi <me@bachwehbi.net>
2017-02-12 01:33:41 +0100

    #29 & #30 add tests and code hardening

---
+++ test/mqtt.js
@@ -0,0 +1,129 @@
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect,
+    bbt = require('../index');
+
+var token = ''
+
+function createConnection() {
+  return new bbt.Connector({
+    apiKey: process.env.APIKEY,
+    secretKey: process.env.SECRETKEY
+  });
+}
+
+function createMqttConnection(ssl) {
+  return new bbt.Stream({transport: {
+    type: 'mqtt',
+    apiKey: process.env.APIKEY,
+    secretKey: process.env.SECRETKEY
+  }});
+}
+
+describe('beebotte.mqtt', function() {
+
+  var mqttclient
+  var msg = null
+
+  function onSub (message) {
+    msg = message.data
+  }
+
+  before(function() {
+    mqttclient = createMqttConnection(true)
+  })
+
+  it('should receive connected event upon connection', function (done){
+
+    var connected = false
+
+    setTimeout(function () {
+      if (!connected) {
+        done('Failed to receive connected event after 5 seconds from connection')
+      }
+    }, 15000)
+
+    mqttclient.on('connected', function() {
+      connected = true
+      done()
+    })
+  })
+
+  it('should subscribe to channel with success', function (done){
+
+    var subscribed = false
+
+    setTimeout(function () {
+      if (!subscribed) {
+        done('Failed to subscribe after 2 seconds from subscription request')
+      }
+    }, 5000)
+
+    mqttclient.on('subscribed', function (sub) {
+      expect(sub.channel).to.be.equal('test')
+      expect(sub.resource).to.be.equal('test')
+      subscribed = true
+      done()
+    })
+
+    mqttclient.subscribe('test', 'test', {read: true, write: true}, onSub)
+  })
+
+  it('should receive published message with success', function (done){
+
+    setTimeout(function () {
+      expect(msg).to.be.equal('msg1')
+      done()
+    }, 500)
+
+    mqttclient.publish('test', 'test', 'msg1')
+  })
+
+  it('should receive written message with success', function (done){
+
+    setTimeout(function () {
+      expect(msg).to.be.equal('msg2')
+      done()
+    }, 500)
+
+    mqttclient.write('test', 'test', 'msg2')
+  })
+
+  it('should unsubscribe from channel with success', function (done){
+
+    var unsubscribed = false
+
+    setTimeout(function () {
+      if (!unsubscribed) {
+        done('Failed to unsubscribe after 2 seconds from unsubscription request')
+      }
+    }, 5000)
+
+    mqttclient.on('unsubscribed', function (sub) {
+      expect(sub.channel).to.be.equal('test')
+      expect(sub.resource).to.be.equal('test')
+      unsubscribed = true
+      done()
+    })
+
+    mqttclient.unsubscribe('test', 'test')
+  })
+
+  it('should disconnect with success', function (done){
+
+    var disconnected = false
+
+    setTimeout(function () {
+      if (!disconnected) {
+        done('Failed to receive disconnected event after 2 seconds from disconnection')
+      }
+    }, 5000)
+
+    mqttclient.on('disconnected', function () {
+      disconnected = true
+      done()
+    })
+
+    mqttclient.disconnect()
+  })
+})
---
+++ test/rest_client.js
@@ -0,0 +1,512 @@
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect,
+    bbt = require('../index')
+
+var token = ''
+  , hostname = 'api.beebotte.com'
+
+function createConnection() {
+  return new bbt.Connector({
+    apiKey: process.env.APIKEY,
+    secretKey: process.env.SECRETKEY
+    hostname: hostname,
+    port: 80
+  })
+}
+
+function createConnectionToken() {
+  return new bbt.Connector({
+    token: token,
+    hostname: hostname,
+    port: 80
+  })
+}
+
+describe('beebotte.rest', function() {
+  this.timeout(15000)
+  var bclient = createConnection()
+
+  it('should create a channel without error', function(done) {
+    bclient.addChannel({
+      name: 'channeltest',
+      label: 'test',
+      description: 'channel test description',
+      ispublic: true,
+      resources: [
+        {name: 'res1', label:'res1', vtype: 'number'},
+        {name: 'res2', description: 'res2',  vtype: 'string'},
+        {name: 'res3', vtype: 'any'}
+      ]
+    }, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      bclient.getChannel('channeltest', function(err, res) {
+        if(err) return done(err)
+        token = res.token
+        expect(res).to.have.property('name')
+        expect(res.name).to.be.equal('channeltest')
+        expect(res.resources[0].vtype).to.be.equal('number')
+        done()
+      })
+    })
+  })
+
+  it('should get existing channel without error', function(done) {
+    bclient.getChannel('channeltest', function(err, res) {
+      if(err) return done(err)
+      expect(res).to.have.property('name')
+      expect(res.name).to.be.equal('channeltest')
+      done()
+    })
+  })
+
+  it('should add a resource without error', function(done) {
+    bclient.addResource({
+      channel: 'channeltest',
+      resource: {
+        name: 'resourcetest',
+        label:'test resource',
+        description: 'some description goes here',
+        vtype: 'string'
+      }
+    }, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should get all existing resources without error - resource parameter set to empty string', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: ''
+    }, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get all existing resources without error - resource parameter set to wildcard character \'*\'', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: '*'
+    }, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get all existing resources without error - resource parameter set to null', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: '*'
+    }, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get all existing resources without error - resource parameter not provided', function(done) {
+    bclient.getResource({
+      channel: 'channeltest'
+    }, function(err, res) {
+      if(err) done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get existing resource without error', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'resourcetest'
+    }, function(err, res) {
+      if(err) done(err)
+      expect(res).to.have.property('channel')
+      expect(res.channel).to.be.equal('channeltest')
+      expect(res).to.have.property('name')
+      expect(res.name).to.be.equal('resourcetest')
+      expect(res).to.have.property('vtype')
+      expect(res.vtype).to.be.equal('string')
+      done()
+    })
+  })
+
+  it('should fail due to resource not found', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'notexist'
+    }, function(err, res) {
+      if(err) return done()
+      done('Get non existing resource succeeded')
+    })
+  })
+
+  it('should fail due to format error', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'abc*'
+    }, function(err, res) {
+      if(err) return done()
+      done('Get a resource with malformed name succeeded')
+    })
+  })
+
+  it('should fail due to format error', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: '*abc'
+    }, function(err, res) {
+      if(err) return done()
+      done('Get a resource with malformed name succeeded')
+    })
+  })
+
+  it('should fail due to format error', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'a*c'
+    }, function(err, res) {
+      if(err) return done()
+      done('Get a resource with malformed name succeeded')
+    })
+  })
+
+  it('should fail due to format error (resource name too short)', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'a'
+    }, function(err, res) {
+      if(err) return done()
+      done('Get a resource with too short resource name succeeded')
+    })
+  })
+
+  it('should delete an existing resource without error', function(done) {
+    bclient.deleteResource({
+      channel: 'channeltest',
+      resource: 'resourcetest'
+    }, function (err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      bclient.getResource({
+        channel: 'channeltest',
+        resource: 'resourcetest'
+      }, function (err, res) {
+        if(err) return done()
+        done('Got resource that was supposed to be deleted')
+      })
+    })
+  })
+
+  it('should fail due to resource not found', function(done) {
+    bclient.getResource({
+      channel: 'channeltest',
+      resource: 'resourcetest'
+    }, function (err, res) {
+      if(err) return done()
+      done('Get resource succeeded')
+    })
+  })
+})
+
+describe('beebotte.rest read/write operations', function() {
+  this.timeout(15000)
+  var bclient = createConnection()
+
+  it('should write to BBT without error', function(done) {
+    bclient.write({channel: 'channeltest', resource: 'res1', data: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should write to BBT 2 records without error', function(done) {
+    bclient.writeBulk({channel: 'channeltest', records: [
+      {resource: 'res1', data: 1},
+      {resource: 'res2', data: '2'},
+    ]}, function(err, res) {
+      if(err) return node(err)
+      expect(res).to.have.length(2)
+      done()
+    })
+  })
+
+  it('should read from BBT without error', function(done) {
+    bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.have.length(1)
+      expect(res[0]).to.have.property('data')
+      expect(res[0].data).to.be.equal(1)
+      done()
+    })
+  })
+
+  it('should delete a record without error', function(done) {
+    //old value was 1, add a new value, delete it then verify you get 1 again
+    bclient.write({channel: 'channeltest', resource: 'res1', data: 111}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+        if(err) return done(err)
+        expect(res).to.have.length(1)
+        expect(res[0]).to.have.property('data')
+        expect(res[0].data).to.be.equal(111)
+        var _id = res[0]._id
+        bclient.delete({channel: 'channeltest', resource: 'res1', _id: _id}, function(err, res) {
+          if(err) return done(err)
+          expect(res).to.have.property('data')
+          expect(res.data).to.be.equal(111)
+          bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+            if(err) return done(err)
+            expect(res).to.have.length(1)
+            expect(res[0]).to.have.property('data')
+            expect(res[0].data).to.be.equal(1)
+            done()
+          })
+        })
+      })
+    })
+  })
+
+  it('should fail deleting due to record not found', function(done) {
+    bclient.delete({channel: 'channeltest', resource: 'res1', _id: '123443213'}, function(err, res) {
+      if(err) return done()
+      done('Delete succeeded')
+    })
+  })
+
+  it('should update a record without error', function(done) {
+    bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+      if(err) return done(err)
+      var _id = res[0]._id
+      bclient.update({channel: 'channeltest', resource: 'res1', _id: _id, data: 5}, function(err, res) {
+        if(err) return done(err)
+        expect(res).to.be.equal(true)
+        bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+          if(err) return done(err)
+          expect(res).to.have.length(1)
+          expect(res[0]).to.have.property('data')
+          expect(res[0].data).to.be.equal(5)
+          done()
+        })
+      })
+    })
+  })
+
+  it('should fail due to resource not found', function(done) {
+    bclient.write({channel: 'channeltest', resource: 'notexist', data: 1}, function(err, res) {
+      if(err) return done()
+      done('Write succeeded')
+    })
+  })
+
+  it('should publish to BBT 2 records without error', function(done) {
+    bclient.publishBulk({channel: 'channeltest', records: [
+      {resource: 'res1', data: 1},
+      {resource: 'res2', data: '2'},
+    ]}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should publish to BBT without error', function(done) {
+    bclient.publish({channel: 'channeltest', resource: 'res1', data: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+})
+
+describe('beebotte.rest read/write operations Token Auth', function() {
+  this.timeout(15000)
+  var bclient
+  before(function() {
+    bclient = createConnectionToken()
+  })
+
+  it('should write to BBT without error', function(done) {
+    bclient.write({channel: 'channeltest', resource: 'res1', data: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should write to BBT 2 records without error', function(done) {
+    bclient.writeBulk({channel: 'channeltest', records: [
+      {resource: 'res1', data: 1},
+      {resource: 'res2', data: '2'},
+    ]}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.have.length(2)
+      done()
+    })
+  })
+
+  it('should read from BBT without error', function(done) {
+    bclient.read({channel: 'channeltest', resource: 'res1', limit: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.have.length(1)
+      expect(res[0]).to.have.property('data')
+      expect(res[0].data).to.be.equal(1)
+      done()
+    })
+  })
+
+  it('should fail due to resource not found', function(done) {
+    bclient.write({channel: 'channeltest', resource: 'notexist', data: 1}, function(err, res) {
+      if(err) return done()
+      done('Write succeeded')
+    })
+  })
+
+  it('should publish to BBT 2 records without error', function(done) {
+    bclient.publishBulk({channel: 'channeltest', records: [
+      {resource: 'res1', data: 1},
+      {resource: 'res2', data: '2'},
+    ]}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should publish to BBT without error', function(done) {
+    bclient.publish({channel: 'channeltest', resource: 'res1', data: 1}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      done()
+    })
+  })
+})
+
+describe('beebotte.rest channel delete', function() {
+  this.timeout(15000)
+  var bclient = createConnection()
+
+  it('should delete an existing channel without error', function(done) {
+    bclient.deleteChannel('channeltest', function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal(true)
+      bclient.getChannel('channeltest', function(err, res) {
+        if(err) return done()
+        done('Got channel that was supposed to be deleted')
+      })
+    })
+  })
+
+  it('should fail due to channel not found', function(done) {
+    bclient.getChannel('channeltest', function(err, res) {
+      if(err) return done()
+      done('Get channel succeeded')
+    })
+  })
+
+  it('should get existing channels without error - channel parameter set to empty string', function(done) {
+    bclient.getChannel('', function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get existing channels without error - channel parameter set to wildcard character \'*\'', function(done) {
+    bclient.getChannel('', function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get existing channels without error - channel parameter set to null', function(done) {
+    bclient.getChannel(null, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get existing channels without error - channel parameter not provided', function(done) {
+    bclient.getChannel(function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('get channel should fail due to format error', function(done) {
+    bclient.getChannel('a*c', function(err, res) {
+      if(err) return done()
+      done('Get a channel with malformed name succeeded')
+    })
+  })
+
+  it('get channel should fail due to format error', function(done) {
+    bclient.getChannel('abc*', function(err, res) {
+      if(err) return done()
+      done('Get a channel with malformed name succeeded')
+    })
+  })
+
+  it('get channel should fail due to format error', function(done) {
+    bclient.getChannel('*abc', function(err, res) {
+      if(err) return done()
+      done('Get a channel with malformed name succeeded')
+    })
+  })
+
+  it('get channel should fail due to channel name too short', function(done) {
+    bclient.getChannel('a', function(err, res) {
+      if(err) return done()
+      done('Get a channel with too short channel name succeeded')
+    })
+  })
+
+})
+
+describe('beebotte.connector user connection management', function() {
+  this.timeout(15000)
+  var bclient = createConnection()
+
+  it('should get user connections with a given userid without error', function(done) {
+    bclient.getUserConnections({userid: '1234567890'}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should get user connections with a given userid and session id without error', function(done) {
+    bclient.getUserConnections({userid: '1234567890', sid: 'abcdefg123456'}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.instanceof(Array)
+      done()
+    })
+  })
+
+  it('should drop user connections with a given userid without error', function(done) {
+    bclient.dropUserConnection({userid: '1234567890'}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal('')
+      done()
+    })
+  })
+
+  it('should drop user connections with a given userid and session id without error', function(done) {
+    bclient.dropUserConnection({userid: '1234567890', sid: 'abcdefg123456'}, function(err, res) {
+      if(err) return done(err)
+      expect(res).to.be.equal('')
+      done()
+    })
+  })
+
+})
